80680b7921a3cff49ac99fec9e8215eea13a72d2
Gustavo Dávila <davila7@utp.edu.co>
2020-11-25 15:20:50 -0500

    ring builded v1

---
+++ client.py
@@ -0,0 +1,221 @@
+import zmq
+import sys
+import json
+import os
+import hashlib
+
+partsize = 1024*1024 #N° MBytes
+
+class Hash():
+    def __init__(self,tipohash):
+        self.tipohash = tipohash
+
+    def getHash(self, file):
+        if self.tipohash == "md5":
+            hasher = hashlib.md5()
+            hasher.update(file)
+            return (hasher.hexdigest())
+        elif self.tipohash == "sha1":
+            hasher = hashlib.sha1(file).hexdigest()
+            return hasher
+        elif self.tipohash == "sha256":
+            hasher = hashlib.sha256(file).hexdigest()
+            return hasher
+        else:
+            print("Don´t found hash type, insert md5,sha1 or sha256")
+            return("NoHash")
+
+class Client():
+    def __init__(self):
+        self.context = zmq.Context()
+        self.hashobj = Hash("sha256")
+
+    def run(self):
+        print("Connecting to server…")
+        socket = self.context.socket(zmq.REQ) #REQ este socket va a ser utilizado para hacer solicitudes
+        
+        cmd = sys.argv[1]
+        if cmd == 'upload':
+            socket.connect("tcp://{}".format(sys.argv[3])) #conexión con el nodo principal 
+            filename = sys.argv[2]
+            ids_nodos = self.idsNodos(socket)
+            #info_send = self.sendInfoProxy(socket,filename)
+            #self.sendDataServers(info_send)
+            socket.close()
+            
+        elif cmd == 'list':
+            socket.connect("tcp://{}".format(sys.argv[3])) #Se conect de modo local, por el pueto indicado en la linea de comandos
+            user = sys.argv[2]
+            self.list(socket,user)
+            socket.close()
+            
+        elif cmd == 'download':
+            socket.connect("tcp://{}".format(sys.argv[4])) 
+            filetodownload = sys.argv[2]
+            user = sys.argv[3]
+            self.download(socket,filetodownload,user)
+            
+        else:
+            print('Error, comando no valido')
+
+    #return 
+    def findSuccesor(self, socket, hash):
+        #main node should send his idnumber and address successor
+        socket.send_multipart([b'idnumber'])
+        ans = socket.recv_multipart()
+        print(ans[1].decode('utf-8'))
+        print(ans[2].decode('utf-8'))"""
+        
+
+    def sendInfoProxy(self,socket,filename):
+        print("enviando info al proxy de {}".format(filename))
+        hash_whole_file = self.complethash(filename)
+        part_hash = self.partHash(filename)
+        user = sys.argv[3]
+        fileinfo = {
+            "user": user,
+            "filename": filename,
+            "parts": part_hash,
+            "complethash": hash_whole_file
+        }
+        #socket.send_json(fileinfo)
+        str_json = json.dumps(fileinfo) #se convierte el json en una cadena
+        socket.send_multipart([b'upload', str_json.encode('utf-8')])
+        resp = socket.recv_json()
+        print(resp)
+        return resp
+
+    def sendDataServers(self, info_send):
+        #TODO  for next implementation: user deleted in json when file already exist, set array of user
+        dir_servers = info_send['servers']
+        dir_servers_new = []
+
+        #lista sin repetir de las direcciones de los servers
+        for address in dir_servers:
+            if not(address in dir_servers_new):
+                dir_servers_new.append(address) 
+
+        print(dir_servers_new)
+        
+        #por cada servidor envio los paquetes correspondientes
+        for i in range(len(dir_servers_new)):
+            socket = self.context.socket(zmq.REQ)
+            socket.connect("tcp://"+dir_servers_new[i])
+            len_array = len(dir_servers)
+            intervalo = len(dir_servers_new) #salto de acuerdo al número de servers que tenga
+            
+            for j in range(i,len_array,intervalo):
+                #print("index: "+str(j))
+                data_bytes = self.readPart(info_send['filename'],j)
+                socket.send_multipart([b'upload', info_send['parts'][j].encode('utf-8'), data_bytes])
+                resp = socket.recv_string()
+                print(resp)
+
+            socket.close() 
+
+    def uniqueAdresses(self, dir_servers):
+        dir_servers_new = []
+        for address in dir_servers:
+            if not(address in dir_servers_new):
+                dir_servers_new.append(address)
+        return dir_servers_new
+
+
+    def list(self,socket,user):
+        socket.send_multipart([bytes("list", encoding='utf-8'),bytes(user, encoding='utf-8')])
+        resp = socket.recv_multipart()
+        for ans in resp:
+            print(ans.decode('utf-8'))
+
+    def download(self,socket,filetodownload,user):
+        # send request to proxy about file client wants download
+        # proxy return server address and parts_hash to download
+        # Group file of each server and download
+        socket.send_multipart([b'download', filetodownload.encode('utf-8'), user.encode('utf-8')])
+        resp_proxy = socket.recv_json()
+        print(resp_proxy)
+        socket.close()
+        #if the dictionary don´t empty
+        if resp_proxy:
+            filename = resp_proxy['filename']
+            dir_servers = resp_proxy['servers']
+            #print(dir_servers)
+
+            dir_servers_new = self.uniqueAdresses(dir_servers)
+
+            #por cada servidor descargo los paquetes correspondientes
+            for i in range(len(dir_servers_new)):
+                socket = self.context.socket(zmq.REQ)
+                socket.connect("tcp://"+dir_servers_new[i])
+                len_array = len(dir_servers)
+                intervalo = len(dir_servers_new) #salto de acuerdo al número de servers que tenga
+                
+                for j in range(i,len_array,intervalo):
+                    socket.send_multipart([b'download', resp_proxy['parts'][j].encode('utf-8')])
+                    resp = socket.recv_multipart()
+                
+                    if resp[0] == b'downloading':
+                        with open('downloads/'+resp_proxy['parts'][j], 'wb') as f:
+                            f.write(resp[1])
+                        print("parte descargada")
+                socket.close()
+            self.buildFile(resp_proxy['parts'])
+            
+            """
+            index = 0
+            for dire in dir_servers:
+                socket = self.context.socket(zmq.REQ)
+                socket.connect("tcp://" + dire)
+                socket.send_multipart([b'download', resp_proxy['parts'][index].encode('utf-8')])
+                resp = socket.recv_multipart()
+                
+                if resp[0] == b'downloading':
+                    with open('downloads/'+filename, 'ab') as f:
+                        f.write(resp[1])
+                    print("parte descargada")
+                index += 1
+                socket.close()
+            print("download ready")"""
+
+        else:
+            print("don´t founded")
+        
+    def complethash(self,filename):
+        #'rb' read binary
+        with open(filename, 'rb') as f:
+            completbytes = f.read() #obtengo todos los bytes
+            hash= self.hashobj.getHash(completbytes) #hash de los bytes
+            return hash
+
+    def partHash(self,filename):
+        hashes = []
+        with open(filename,'rb') as f:
+            while True:
+                partbytes = f.read(partsize) #leo solo una parte del archivo (partsize=1M)
+                if not partbytes:
+                    break
+                parthash= self.hashobj.getHash(partbytes)
+                hashes.append(parthash)
+            return hashes
+    
+    def readPart(self,filename, index):
+        bytes = 0
+        with open(filename, 'rb') as f:
+            f.seek(partsize*index)
+            bytes = f.read(partsize)
+        return bytes
+    
+    #TODO do function
+    def buildFile(self, parthash):
+        pass
+        """
+        with open(filename, 'rb') as f:
+            completbytes = f.read() #obtengo todos los bytes
+            hash= self.hashobj.getHash(completbytes) #hash de los bytes
+            return hash"""
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.run()
+    
---
+++ nodo.py
@@ -0,0 +1,312 @@
+#comand
+# python nodo.py  none localhost:4000 6 20 alpha
+
+#node client
+# python nodo.py localhost:4000 localhost:5000 6 40 none
+
+#node 2 client
+# python nodo.py localhost:5000 localhost:6000 6 30 none
+
+import zmq
+import os
+import json
+import sys
+from random import choice
+import string
+import hashlib
+import math
+
+class FServer():
+    #en los argumentos recivir la carpeta donde va a se almacenado todo
+    def __init__(self):
+        self.context = zmq.Context() #nos permite crear el socket
+
+    def run(self):
+        validation = self.receiveParameters()
+        if validation == True:
+            address_to_connect = sys.argv[1]
+            self.ip_and_port = sys.argv[2]
+            self.ip_server = self.ip_and_port.split(':')[0]
+            self.port_server = self.ip_and_port.split(':')[1]
+            self.bits = sys.argv[3]
+            self.number_server = sys.argv[4]
+            self.type_server = sys.argv[5]
+
+            #random_str = self.randomString(self.ip_and_port)
+            #self.id_server = int(self.hashString(random_str),16)
+            #self.number_server = "37893173180865016123064796019639841985096597669"
+
+            
+            socket = self.initSocket()
+            if self.type_server == "alpha":
+                print("First/main node")
+                self.saveServer(self.ip_and_port, self.ip_and_port)
+                self.receive(socket)
+            else:
+                print("join to ring")
+                self.saveServer()
+                self.findSuccessor(address_to_connect)
+                self.receive(socket)
+
+
+    def receiveParameters(self):
+        boolean = True
+        try:
+            sys.argv[1]
+        except:
+            print("Input ip_server:port_server to connect")
+            boolean = False
+        try:
+            sys.argv[2]
+        except:
+            print("Input Own ip_server:port_server")
+            boolean = False
+        try:
+            sys.argv[3]
+        except:
+            print("Input bits")
+            boolean = False
+        try:
+            sys.argv[4]
+        except:
+            print("Input id/number of server")
+            boolean = False
+        try:
+            sys.argv[5]
+        except:
+            print("Input node type if first node: type is alpha")
+            boolean = False
+        return boolean
+        
+
+    def initSocket(self):
+        socket = self.context.socket(zmq.REP) #REP(REPLY) to answer to clients
+        socket.bind("tcp://*:{}".format(self.port_server)) #se enlaza por medio del protocolo tcp y va a responder todo lo que venga del pueto 5555
+        print("Socket created in port {}!!!".format(self.port_server))
+        return socket
+
+    def saveServer(self, successor="", predecessor=""):
+        first_upper_limit = math.pow(2,int(self.bits))
+        print(first_upper_limit)
+        info_server = {
+            "id_server": self.number_server,
+            "ip": self.ip_server,
+            "port": self.port_server,
+            "successor": successor,
+            "number_successor": self.number_server, 
+            "predecessor": predecessor,
+            "number_predecessor": self.number_server         
+        }
+        f = open('info_server.json','w')
+        #servers_dict[self.id_server] = info_server
+        json.dump(info_server, f, indent=4)
+        f.close()    
+    
+    def findSuccessor(self, address_to_connect):
+        #code range of responsability
+        #recorrer anillo hasta conectarse
+        print("connect with Ring..")
+        socket = self.context.socket(zmq.REQ)
+        socket.connect("tcp://{}".format(address_to_connect))
+        #print("conectado")
+        #while resp[0] == 'ok' seguir buscando
+        socket.send_multipart([b'ringconnect', self.number_server.encode('utf-8'), self.ip_and_port.encode('utf-8')])
+        resp = socket.recv_json()
+
+        if resp['first'] == 'true':
+            print("two node in the ring")
+            f = open('info_server.json','r')
+            servers_dict = json.load(f)
+            f.close()
+            servers_dict['predecessor'] = resp['ip']
+            servers_dict['number_predecessor'] = resp['number']
+            servers_dict['successor'] = resp['ip']
+            servers_dict['number_successor'] = resp['number']
+            f = open('info_server.json','w')
+            json.dump(servers_dict, f, indent=4)
+            f.close()
+        elif resp['response'] == 'true':
+            print("find responsible")
+            ip_predecessor = resp['predecessor']
+            number_predecessor = resp['number_predecessor']
+            f = open('info_server.json','r')
+            servers_dict = json.load(f)
+            f.close()
+            servers_dict['successor'] = resp['ip'] # es necesario  actualizar el predecessor en el nodo que hace la solicitud para el caso donde se tengan que reparitir los archivos
+            servers_dict['number_successor'] = resp['number']
+            servers_dict['predecessor'] = ip_predecessor
+            servers_dict['number_predecessor'] = number_predecessor
+
+            f = open('info_server.json','w')
+            json.dump(servers_dict, f, indent=4)
+            f.close()
+            socket.close()
+
+            socket = self.context.socket(zmq.REQ)
+            print("ip predecessor: "+ ip_predecessor) 
+            socket.connect("tcp://{}".format(resp['predecessor']))
+            socket.send_multipart([b'newsuccessor', self.ip_and_port.encode('utf-8'), self.number_server.encode('utf-8')])
+            ans = socket.recv_json()
+            print(ans)
+
+        elif resp['response'] == 'false':
+            print("find new responsible")
+            #TODO do recursive call to findSuccessor()
+        
+
+        
+        print(resp)      
+
+
+    def receive(self,socket):
+        print("waiting messages..")
+        while True:
+            message = socket.recv_multipart()
+
+            if message[0] == b'ringconnect':
+                #TODO como se un numero me corresponde?
+                #se debe tener el número del predecesor para poder comparar
+                number_node = message[1].decode('utf-8')
+                address_request = message[2].decode('utf-8')
+                self.isMyRange(socket,number_node, address_request)
+            elif message[0] == b'newsuccessor':
+                ip_successor = message[1].decode('utf-8')
+                number_successor = message[2].decode('utf-8')
+                self.updateSuccessor(socket, ip_successor, number_successor)
+
+            elif message[0] == b'idnumber':
+                self.idNumber(socket)
+
+            elif message[0] == b'upload':
+    
+                name_parthash = message[1].decode('utf-8')
+                print("name_parthash: {}".format(name_parthash))
+                        
+                dirserver = "D:\Escritorio\Arquitectura cliente servidor\code/files/"+self.name_server+"/"
+                print(self.name_server)
+                
+                with open(dirserver + name_parthash, 'wb') as f:
+                    f.write(message[2])
+                    socket.send_string("Part upload!!")
+
+            elif message[0] == b'download':
+                part_hash = message[1].decode('utf-8')
+                print(part_hash)
+                print(self.name_server)
+                dir_server = "D:\Escritorio\Arquitectura cliente servidor\code/files/"+self.name_server+"/"
+            
+                try:
+                    with open(dir_server+ part_hash,'rb') as f:
+                        bytes = f.read()
+                        socket.send_multipart([b"downloading", bytes])
+                except:
+                    socket.send_multipart([b"Notfound"])
+            else:
+                print('Error!!')
+                socket.send_string("Error")
+
+    def isMyRange(self,socket, number_node, address_request):
+        print('address request' + address_request)
+        print("Enter ismyrange fuction")
+        f = open('info_server.json','r')
+        servers_dict = json.load(f)
+        f.close()
+        number_predecessor = int(servers_dict['number_predecessor'])
+        number_successor = int(servers_dict['number_successor'])
+        ip_predecessor = servers_dict['predecessor']
+        ip_successor = servers_dict['successor']
+        id_server = int(servers_dict['id_server'])
+        
+        #TODO this code put in a function
+        #verified if node is first in connect to chord
+        if (number_predecessor == int(self.number_server)) and (number_successor == int(self.number_server)):
+            print("First node in connect")
+            f = open('info_server.json','w')
+            servers_dict['predecessor'] = address_request
+            servers_dict['successor'] = address_request
+            servers_dict['number_predecessor'] = number_node
+            servers_dict['number_successor'] = number_node
+            json.dump(servers_dict, f, indent=4)
+            f.close()
+            #TODO in node request, update info_server too
+            socket.send_json({"response": "true", "successor": "", "ip": self.ip_and_port, "number": self.number_server, "first": 'true'})
+        
+        else:
+
+            if int(number_node) > number_predecessor and int(number_node) <= id_server:
+                print("esta en el rango")
+                f = open('info_server.json','w')
+                servers_dict['predecessor'] = address_request
+                servers_dict['number_predecessor'] = number_node
+                json.dump(servers_dict, f, indent=4)
+                f.close()
+                socket.send_json({"response": "true", "successor": "", "ip": self.ip_and_port, "number": self.number_server, "first": 'false', "predecessor": ip_predecessor, "number_predecessor": number_predecessor})
+            else:
+                print("no esta en el rango")
+                socket.send_json({"response": "false", "ip_successor": ip_successor, "first": 'false'}) 
+
+    def updateSuccessor(self, socket, ip_successor, number_successor):
+        f = open('info_server.json','r')
+        servers_dict = json.load(f)
+        f.close()
+        f = open('info_server.json','w')
+        servers_dict['successor'] = ip_successor
+        servers_dict['number_successor'] = number_successor
+        json.dump(servers_dict, f, indent=4)
+        f.close() 
+        socket.send_json({"response": "update ok"})
+
+
+    def idNumber(self, socket):
+        f = open('info_server.json','r')
+        servers_dict = json.load(f)
+        id_server = servers_dict['id_server']
+        address_successor = servers_dict['successor']
+        socket.send_multipart([b'info_server', id_server.encode('utf-8'), address_successor.encode('utf-8')])
+        f.close()
+
+    def randomString(self, s = '', n =30):
+        chars = string.ascii_uppercase + string.ascii_lowercase + '0123456789'
+        return s + ''.join(choice(chars) for i in range(30))
+    
+    def hashString(self, s):
+        sha1 = hashlib.sha1()
+        sha1.update(s.encode('utf-8'))
+        return sha1.hexdigest()
+
+    def rangeResponsibility(self, ids):
+        for i in ids:
+            print(i)
+        
+        print("------")
+        resp = [(ids[-1],ids[0])]
+        for i in range(1,len(ids)):
+            resp.append((ids[i-1],ids[i]))
+        
+        for i in range(len(resp)):
+            print("{} resp {}".format(i,resp[i]))
+
+if __name__ == "__main__":
+    server = FServer()
+    #proxy.servers()
+    server.run()
+    #TODO cada server debe tener su nombre aleatorio
+    #TODO poner esto en el cliente
+
+    """"
+    ip = '192.236.2.78:2500'
+    sids = [server.randomString(ip) for x in range(3)]
+    ids = [int(server.hashString(s),16) for s in sids]
+    ids.sort()
+
+    server.rangeResponsibility(ids)
+    #server 0: 37893173180865016123064796019639841985096597669
+    #server 1: 515748800960014143714155257729430307158535308957
+    #server 2: 647584871384597889933400031157176508445575663970
+
+    
+    Range Responsability
+    0 resp (647584871384597889933400031157176508445575663970, 37893173180865016123064796019639841985096597669)
+    1 resp (37893173180865016123064796019639841985096597669, 515748800960014143714155257729430307158535308957)
+    2 resp (515748800960014143714155257729430307158535308957, 647584871384597889933400031157176508445575663970)
+    """
